01317b9ea5a6b847478756fb1d94768b304b60f3
Paulus Linschoten Framis <plinscho@cbr7s6.42barcelona.com>
2025-06-04 19:01:34 +0200

    fixed input logic

--- srcs/predict.py
+++ srcs/predict.py
@@ -21,35 +21,15 @@ dict_km_price = {}
 class PredictPrice():
     def __init__(self, data="./data.csv"):
         # Init parameters
-        self.learning_rate = 0.001
+        self.learning_rate = 0.01
         self.t0 = 0
         self.t1 = 0
 
-        # Init data
+        # Init data (its divided by 1000, carefull)
         self.data_raw = read_data(data)
         self.km_raw = [row[0] for row in self.data_raw]
         self.price_raw = [row[1] for row in self.data_raw]
         
-        # Init statistics stuff 
-        self.average_km = sum(self.km_raw) / len(self.km_raw)
-
-        # get_std_dev does not exitst !! self.std_dev_km = get_std_dev(self.average_km, self.km_raw)
-        self.std_dev_km = 1
-        self.m = len(self.data_raw)
-
-        # Scale data
-        self.km = [
-            (raw_km - self.average_km) / self.std_dev_km
-            for raw_km in self.km_raw
-        ]
-
-        # Acuracy loss
-        self.acc_loss = []
-
-    
-
-
-
 def read_data(path):
     first_line = True;
     global dict_km_price
@@ -84,13 +64,18 @@ def print_data():
 
 
 def get_km_user():
-    miles_str = input("Write a mileage to get a price: ")
-    miles = int(miles_str)
-    if miles < 0:
-        print("Enter a positive value...")
-        return get_km_user()
-    else:
-        return miles
+    ok = False;
+    while (ok == False):
+        try:
+            miles_str = input("Write a mileage to get a price: ")
+            miles = int(miles_str)
+            if miles < 0:
+                print("Enter a positive value...")
+            else:
+                ok = True
+                return miles
+        except:
+            print("Error.\nIntroduce a valid positive int")
 
 def predict_price(km):
    print(km) 
