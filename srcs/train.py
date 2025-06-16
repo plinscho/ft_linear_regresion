5bb96a4594b2c6b98bb1c956e25ff84624217804
Paulus Linschoten Framis <plinscho@cbr3s2.42barcelona.com>
2025-06-16 17:46:00 +0200

    started again

--- srcs/train.py
+++ srcs/train.py
@@ -1,44 +1,32 @@
-import numpy as np
-import matplotlib as mp
-from utils import read_data, print_data
-
 """
-    repeat until convergence
-{
-    w = w - (learning_rate * (dJ/dw))
-    b = b - (learning_rate * (dJ/db))
-}
-
-    where w = 
-    where b = 
 
 """
 
-class Train:
-    def  __init__(self):
-        self.raw_data = read_data('srcs/data.csv')
-        self.km = [km for km, prices in self.raw_data.items()]
-        self.prices = [price for km, price in self.raw_data.items()]
-        self.t0 = 0                 # theta1
-        self.t1 = 0                 # theta0
-        self.learn_rate = 0.001     # Weight of each iteration
-        self.epochs = 100           # Number of iterations
+from utils import print_data
 
-def mean_squared_error(data :Train):
-    y = data.prices
-
-def calc_theta(data :Train):
-    return ""
-    
-def calc_mse(data :Train):
-    return ""
+def read_data(source="./data.csv"):
+    data = {}
+    f = open(source)
+    fl = False
+    for line in f:
+        if fl is False:
+            fl = True
+            continue
+        line = line.strip()
+        split = line.split(',')
+        try:
+            miles = int(split[0])
+            price = int(split[1])
+            data[miles] = price
+        except:
+            print(f"Error in line: {line}")
+    f.close()
+    return data
 
 
 def main():
-    train = Train()
-    print(train.raw_data)
-    print(len(train.km))
-    print(train.prices)
-    print(len(train.prices))
+    data = read_data()
+    print_data(data)
+    return
 
 main()
--- srcs/utils.py
+++ srcs/utils.py
@@ -1,32 +1,4 @@
-def read_data(path):
-    first_line = True;
-    global dict_km_price
-    raw_km_price = {}
-
-    with open(path, 'r') as file:
-        for line in file:
-            line = line.strip() # Remove white spaces and newlines
-
-            if first_line:
-                first_line = False
-                continue
-
-            data = line.split(',')
-            if len(data) != 2:
-                print(f"Skipping invalid line: {line}")
-                continue
-            try:
-                km = int(data[0])
-                price = int(data[1])
-                raw_km_price[km] = price
-            except:
-                print(f"Error in line: {line}")
-
-    dict_km_price = dict(sorted(raw_km_price.items()))
-    print("Data loaded succesfully!")
-    return dict_km_price
 
 def print_data(data):
-    print(f"KM --> Price:")
-    for km, price in data.items():
-        print(f"{km}\tkm ---> {price}€")
+    for miles, price in data.items():
+        print(f"miles: {miles} \t| price: {price}")
